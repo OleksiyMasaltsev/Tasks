f63c67861333750e6006ab261a93f9dcdbc6dd6d
Masaltsev <a.o.masaltsev@gmail.com>
2022-04-02 00:06:37 +0300

    computer class

--- src/main/java/ua/masaltsev/home/Main.java
+++ src/main/java/ua/masaltsev/home/Main.java
@@ -1,7 +1,13 @@
 package ua.masaltsev.home;
 
+import ua.masaltsev.opt.Computer;
+
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
+        Computer computer = new Computer("Blaster");
+        Optional<Computer> optionalComputer = Optional.of(computer);
 
     }
 }
---
+++ src/main/java/ua/masaltsev/opt/Computer.java
@@ -0,0 +1,13 @@
+package ua.masaltsev.opt;
+
+public class Computer {
+    private String soundCardName;
+
+    public Computer(String soundCardName) {
+        this.soundCardName = soundCardName;
+    }
+
+    public String getSoundCardName() {
+        return soundCardName;
+    }
+}
