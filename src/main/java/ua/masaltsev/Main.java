4b9c46887df94a076457a1109a5465ecd06adff5
Masaltsev <a.o.masaltsev@gmail.com>
2022-04-03 19:22:54 +0300

    ValidPalindromeII

--- src/main/java/ua/masaltsev/Main.java
+++ src/main/java/ua/masaltsev/Main.java
@@ -1,16 +1,14 @@
 package ua.masaltsev;
 
 import ua.masaltsev.exercises.TwoSum;
+import ua.masaltsev.exercises.ValidPalindromeII;
 import ua.masaltsev.opt.Computer;
 
 import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        int[] input = {3, 2, 4};
-        for (int i:
-                new TwoSum().twoSum(input, 6)) {
-            System.out.print(i + " ");
-        }
+        String s = "abeca";
+        System.out.println(new ValidPalindromeII().validPalindrome(s));
     }
 }
