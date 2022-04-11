ea7dc9ba7c022527903c9260d0c10f225156e096
masaltsev <a.o.masaltsev@gmail.com>
2022-04-12 01:19:28 +0300

    User blocks task

---
+++ src/main/java/ua/masaltsev/userblocks/UserBlocksTask.java
@@ -0,0 +1,75 @@
+/**
+ * Implement a service that receives a text block
+ * (a string with multiple lines split by '\n')
+ * and returns a list of the user objects created out of it.
+ *
+ * Requirements:
+ * - User is a POJO with two string fields: name and email.
+ * - Each line must be validated for containing exactly one email and a name.
+ * - Line format might be one of following:
+    "name email"
+    "email name"
+ * - Validate email using EmailValidator by Apache Commons.
+ * - The same email should not appear in different lines.
+ *
+ * If an invalid data arrives, an exception must be thrown with the description of which line contains corrupted data.
+ * Write unit tests to cover all possible test cases.
+ */
+
+package ua.masaltsev.userblocks;
+
+import org.apache.commons.validator.routines.EmailValidator;
+
+import java.util.*;
+
+public class UserBlocksTask {
+
+    private static final List<User> userList = new ArrayList<>();
+
+    public static List<User> extractUsersFrom(String input) throws WrongInputException {
+
+        String[] inputArray = input.split("\n");
+        int lineCounter = 0;
+
+        for (String inputLine : inputArray) {
+            lineCounter++;
+            String[] inputLineArray = inputLine.split(" ");
+            String s1 = inputLineArray[0];
+            String s2 = inputLineArray[inputArray.length-1];
+            userList.add(getValidUser(s1, s2, lineCounter));
+        }
+
+        return userList;
+    }
+
+    private static User getValidUser(String s1, String s2, int lineCounter) throws WrongInputException {
+
+        EmailValidator validator = EmailValidator.getInstance();
+
+        if (validator.isValid(s1) && validator.isValid(s2)) {
+            throw new WrongInputException("Wrong input data in line " + lineCounter);
+        }
+        if (!validator.isValid(s1) && !validator.isValid(s2)) {
+            throw new WrongInputException("Wrong input data in line " + lineCounter);
+        } else if (validator.isValid(s1)){
+            return new User(s2, s1);
+        } else {
+            return new User(s1, s2);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/ua/masaltsev/userblocks/UserBlocksTest.java
@@ -0,0 +1,17 @@
+package ua.masaltsev.userblocks;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserBlocksTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void extractUsersFrom() {
+    }
+}
