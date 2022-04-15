db4f4afb0c6237489fc07ffde6e650d056d3a556
Masaltsev <a.o.masaltsev@gmail.com>
2022-04-15 21:35:51 +0300

    user blocks and tests

--- src/main/java/ua/masaltsev/Main.java
+++ src/main/java/ua/masaltsev/Main.java
@@ -1,11 +1,5 @@
 package ua.masaltsev;
 
-import ua.masaltsev.exercises.TwoSum;
-import ua.masaltsev.exercises.ValidPalindromeII;
-import ua.masaltsev.opt.Computer;
-
-import java.util.Optional;
-
 public class Main {
     public static void main(String[] args) {
 
