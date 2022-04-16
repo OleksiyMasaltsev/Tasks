91cb7e18165108e6b432323bde24eee0ffe781a6
Masaltsev <a.o.masaltsev@gmail.com>
2022-04-17 02:07:45 +0300

    improvements

--- src/main/java/ua/masaltsev/userblocks/User.java
+++ src/main/java/ua/masaltsev/userblocks/User.java
@@ -1,27 +1,11 @@
 package ua.masaltsev.userblocks;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class User {
     private String name;
     private String email;
-
-    public User(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
