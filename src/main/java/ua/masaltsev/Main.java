7e22cc77d1abbcdc06018f127a2302046cc09862
Masaltsev <a.o.masaltsev@gmail.com>
2022-04-17 11:46:02 +0300

    lombok

--- src/main/java/ua/masaltsev/Main.java
+++ src/main/java/ua/masaltsev/Main.java
@@ -1,7 +1,24 @@
 package ua.masaltsev;
 
+import ua.masaltsev.userblocks.User;
+import ua.masaltsev.userblocks.UserBlocksTask;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Main {
     public static void main(String[] args) {
-
+//        String block = "Jack jack@yahoo.com\n" +
+//                "oksi_23@ukr.net Ieromenko Oksi\n" +
+//                "oksi_23@ukr.net Ieromenko Oksi\n" +
+//                "Oleksiy Olegovitch Masaltsev a.o.masaltsev@gmail.com";
+//
+//        UserBlocksTask task = new UserBlocksTask();
+//        List<User> resultList = task.extractUsersFrom(block);
+//
+//        for (User user : resultList) {
+//            System.out.println(user.getName() + " - " + user.getEmail());
+//        }
     }
 }
--- src/main/java/ua/masaltsev/userblocks/User.java
+++ src/main/java/ua/masaltsev/userblocks/User.java
@@ -1,10 +1,13 @@
 package ua.masaltsev.userblocks;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
+//@Builder
+@Data
+//@NoArgsConstructor
 @AllArgsConstructor
 public class User {
     private String name;
