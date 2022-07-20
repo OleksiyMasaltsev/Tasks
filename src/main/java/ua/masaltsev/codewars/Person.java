d9e192e5b9fc4e5f71f7b0ed8fec8da97310925b
Masaltsev <a.o.masaltsev@gmail.com>
2022-07-20 13:19:58 +0300

    TheOffice

---
+++ src/main/java/ua/masaltsev/codewars/Person.java
@@ -0,0 +1,11 @@
+package ua.masaltsev.codewars;
+
+class Person {
+    String name;    // team memnber's name
+    int happiness;  // happiness rating out of 10
+
+    public Person(String name, int happiness) {
+        this.name = name;
+        this.happiness = happiness;
+    }
+}
